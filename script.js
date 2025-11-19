191f011020663a45053722e007343fda7ea2ba37
cychobby <cychobby@gmail.com>
2025-11-19 12:52:58 +0800

    2nd commit [Features] Typewriter & Loop Playback Effect

--- script.js
+++ script.js
@@ -8,3 +8,59 @@ projectItems.forEach(item => {
         }
     });
 });
+
+// Typewriter effect
+const typewriterElement = document.querySelector(".typewriter");
+const texts = ["CS junior", "LLM user", "Freshman of Full stack development"];
+let textIndex = 0;
+let isDeleting = false;
+
+function typeWriter() {
+    const currentText = texts[textIndex];
+    const typingSpeed = 100; 
+    const deletingSpeed = 50; 
+    const pauseAfterTyping = 2000;
+    const pauseAfterDeleting = 500;
+    
+    if (!isDeleting) {
+    // typing phase
+    let charIndex = 0;
+    typewriterElement.textContent = '';
+    
+    const typingInterval = setInterval(() => {
+        typewriterElement.textContent += currentText[charIndex];
+        charIndex++;
+        
+        if (charIndex === currentText.length) { // finished typing
+        clearInterval(typingInterval);
+        // pause after typing, then start deleting
+        setTimeout(() => {
+            isDeleting = true;
+            typeWriter();
+        }, pauseAfterTyping);
+        }
+    }, typingSpeed);
+    
+    } else {
+    // deleting phase
+    let charIndex = currentText.length;
+    
+    const deletingInterval = setInterval(() => {
+        typewriterElement.textContent = currentText.substring(0, charIndex - 1);
+        charIndex--;
+        
+        if (charIndex === 0) {
+        clearInterval(deletingInterval);
+        isDeleting = false;
+        textIndex = (textIndex + 1) % texts.length;
+        
+        setTimeout(() => {
+            typeWriter();
+        }, pauseAfterDeleting);
+        }
+    }, deletingSpeed);
+    }
+}
+
+// Start the typewriter effect
+typeWriter();
